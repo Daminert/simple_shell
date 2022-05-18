399d6b896ffff0791ce4ccc60fe16efc3b44c95e
Olaife Olawore <olaifeolawore@gmail.com>
2022-05-18 12:21:37 -0700

    modified aux_error2.c

--- aux_error2.c
+++ aux_error2.c
@@ -2,6 +2,7 @@
 
 /**
  * error_env - error message for env in get_env.
+ *
  * @datash: data relevant (counter, arguments)
  * Return: error message.
  */
@@ -37,6 +38,7 @@ char *error_env(data_shell *datash)
 }
 /**
  * error_path_126 - error message for path and failure denied permission.
+ *
  * @datash: data relevant (counter, arguments).
  *
  * Return: The error string.
