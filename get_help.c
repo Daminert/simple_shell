ddf809f8f232697edb2ab7d951125c062b6caa37
Olaife Olawore <olaifeolawore@gmail.com>
2022-05-18 13:18:25 -0700

    get_help done

--- get_help.c
+++ get_help.c
@@ -2,7 +2,9 @@
 
 /**
  * get_help - function that retrieves help messages according builtin
+ *
  * @datash: data structure (args and input)
+ *
  * Return: Return 0
 */
 int get_help(data_shell *datash)
