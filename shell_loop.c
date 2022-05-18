09a2a3e34ae91652d34b5d548d0002c43ae027ac
Olaife Olawore <olaifeolawore@gmail.com>
2022-05-18 13:37:42 -0700

    shell_loop.c modified and completed

--- shell_loop.c
+++ shell_loop.c
@@ -4,6 +4,7 @@
  * without_comment - deletes comments from the input
  *
  * @in: input string
+ *
  * Return: input without comments
  */
 char *without_comment(char *in)
@@ -37,6 +38,7 @@ char *without_comment(char *in)
 
 /**
  * shell_loop - Loop of shell
+ *
  * @datash: data relevant (av, input, args)
  *
  * Return: no return.
